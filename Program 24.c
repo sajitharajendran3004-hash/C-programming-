810ed017bdb23033272d05f08f0b7f3caad491e7
sajitharajendran3004-hash <sajitharajendran3004@gmail.com>
2026-02-18 13:53:27 +0530

    Create Program 24.c

---
+++ Program 24.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+
+    printf("Octal = %o\n", n);
+    printf("Hexadecimal = %X", n);
+
+    return 0;
+}
