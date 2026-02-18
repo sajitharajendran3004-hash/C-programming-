c9a837501780c80e982693cad01ac730f4191cf6
sajitharajendran3004-hash <sajitharajendran3004@gmail.com>
2026-02-18 13:56:31 +0530

    Create Program 30.c

---
+++ Program 30.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter number: ");
+    scanf("%d",&n);
+
+    if(n%2==0) printf("Even Number");
+    else printf("Odd Number");
+
+    return 0;
+}
