be5251745bf92868dfb1b8f41c88190a29a15a7e
sajitharajendran3004-hash <sajitharajendran3004@gmail.com>
2026-02-28 12:01:04 +0530

    Add engine health assessment program

---
+++ program 43.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    float temp, oil;
+    int rpm;
+
+    printf("Enter Temperature (C): ");
+    scanf("%f", &temp);
+    printf("Enter Oil Pressure (PSI): ");
+    scanf("%f", &oil);
+    printf("Enter RPM: ");
+    scanf("%d", &rpm);
+
+    if (temp > 75 || oil < 12 || rpm > 7000)
+        printf("Engine Health: Poor");
+    else if (temp > 50 || oil < 12 || rpm > 4000)
+        printf("Engine Health: Fair");
+    else
+        printf("Engine Health: Good");
+
+    return 0;
+}
