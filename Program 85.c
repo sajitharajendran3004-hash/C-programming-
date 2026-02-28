fda13cbdee8d8a3fe04c9c9586933cb0917196a2
sajitharajendran3004-hash <sajitharajendran3004@gmail.com>
2026-02-28 15:53:16 +0530

    Create Program 85.c

---
+++ Program 85.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int start;
+    scanf("%d",&start);
+
+    if(start<=5)
+        printf("Will Complete");
+    else
+        printf("Cannot Complete");
+
+    return 0;
+}
